95d16686bc2e55be0c8be1549dbe1ed3f5306bd8
David Gallego Gomez <davizs@hotmail.com>
2019-04-19 19:39:46 +0200

    Creada navbar y register (faltan estilos y pruebas de funcionamiento)

--- myapp/controllers/auth.controller.js
+++ myapp/controllers/auth.controller.js
@@ -1,21 +1,43 @@
 const mongoose = require('mongoose');
 const Game = require('../models/game.model')
+const User = require('../models/user.model')
 
 module.exports.index = ((req, res, next) => {
   Game.find()
     .then(games =>  {
-      res.render('index', { title: 'Boardgamia games', games })
+      res.render('index', { title: 'BoardGamia games', games })
     })
     .catch(next)
 });
 
 module.exports.register = ((req, res, next) => {
-      res.render('auth/register', { title: 'Register'})
+  res.render('auth/register', { title: 'Register'})
 });
 
 module.exports.doRegister = ((req, res, next) => {
-  res.render('auth/register', { title: 'Register'})
-});
+  
+  function renderWithErrors(errors) {
+    res.render('auth/register', { 
+      user: req.body,
+      errors,
+      title: 'Register'})
+  }
+
+  User.findOne( { email: req.body.mail } ) // AND con el username
+    .then( user => {
+      if (user) { renderWithErrors( { email: 'Email already registered' } ) }
+      else { 
+        user = new User(req.body);
+        return user.save()
+          .then( user => res.redirect('/login') )
+      }
+    })
+    .catch( error => {
+      if (error instanceof mongoose.Error.validationError) { renderWithErrors(error.errors) }
+      else { next(error) }
+    } )
+  }
+);
 
 
 module.exports.login = ((req, res, next) => {
