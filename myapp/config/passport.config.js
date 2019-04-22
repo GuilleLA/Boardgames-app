8e96d921cd14b9bc5583e30f3546e00415bdc36a
Guille LA <guillermolucena90@gmail.com>
2019-04-22 16:32:48 +0200

    passport local auth configurated

--- myapp/config/passport.config.js
+++ myapp/config/passport.config.js
@@ -6,3 +6,31 @@ const LocalStrategy = require('passport-local').Strategy;
 passport.serializeUser((user, next) => {
   next(null, user.id)
 })
+
+passport.deserializeUser((id, next)  => {
+  User.findById(id)
+    .then(user => next(null, user))
+    .catch(next)
+})
+
+passport.use("local-auth", new LocalStrategy({
+  usernameField: 'username',
+  passwordField: 'password'
+}, (username, password, next) => {
+  User.findOne({username: username})
+    .then(user => {
+      if(!user){
+        next(null, null, {password: 'Invalid username or password'})
+      } else {
+        return user.checkpassword(password)
+          .then(match => {
+            if(!match){
+              next(null, null, {password: 'Invalid username or password'})
+            } else {
+              next(null, user)
+            }
+          })
+      }
+    })
+}))
+
