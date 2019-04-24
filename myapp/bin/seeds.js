44890a622a6b0f8a2173b3d8cdf388b9e08b6cc3
Guille LA <guillermolucena90@gmail.com>
2019-04-24 19:21:50 +0200

    added seeds, added npm package to eliminate html tags from games description

--- myapp/bin/seeds.js
+++ myapp/bin/seeds.js
@@ -2,10 +2,18 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const Game = require('../models/game.model')
 const games = require('../data/games.json')
+const striptags = require('striptags')
+
+const correctedGames =  games.map(item => {
+  item.description = striptags(item.description)
+  return item
+})
 
 require('../config/db.config')
 
-Game.create(games)
+
+
+Game.create(correctedGames)
   .then(games => console.info(`${games.length} new games added to database`))
   .catch(error => console.error(error))
   .then(()=> mongoose.connection.close())
