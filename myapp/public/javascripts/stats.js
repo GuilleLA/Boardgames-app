d130af177243c1bcc5cdc10307cad455c4fc0bd1
David Gallego Gomez <davizs@hotmail.com>
2019-05-11 17:22:01 +0200

    Ages Chart

--- myapp/public/javascripts/stats.js
+++ myapp/public/javascripts/stats.js
@@ -2,39 +2,22 @@ window.onload = function() {
   axios.get(`/stats/coordinates`)
   var ctx = document.getElementById('myChart');
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'doughnut',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
+        labels: ["Less 20 years", "Between 20 and 30 years", "Between 30 and 40 years", "Above 40 years"],
+        datasets: [
+          {
+            label: "Age (years)",
+            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
+            data: [2478,5267,734,7824]
+          }
+        ]
+      },
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+      title: {
+        display: true,
+        text: 'Users Age'
+      }
     }
 });
 }
