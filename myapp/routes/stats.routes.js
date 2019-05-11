d94c1cf994e9660adbb43620a63fdd80b1f763b9
Guille LA <guillermolucena90@gmail.com>
2019-05-11 17:20:29 +0200

    created methods to seend data to the graphs

--- myapp/routes/stats.routes.js
+++ myapp/routes/stats.routes.js
@@ -7,5 +7,7 @@ const secure        = require('../middlewares/secure.mid');
 /* GET users listing. */
 router.get('/', secure.isAuthenticated, statsController.stats);
 router.get('/coordinates', secure.isAuthenticated, statsController.coordinates);
+router.get('/ownedMost', secure.isAuthenticated, statsController.ownedMost);
+router.get('/ages', secure.isAuthenticated, statsController.ages);
 
 module.exports = router;
